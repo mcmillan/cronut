59c71913f5dd52ed66943edcc034354184c13eb9
Josh McMillan <josh@joshmcmillan.co.uk>
2017-08-12 18:10:08 +0100

    Include invalid segment in error message

--- lib/cronut/parser.rb
+++ lib/cronut/parser.rb
@@ -13,7 +13,7 @@ module Cronut
       when /^[0-9]+$/
         klass = Cronut::Parser::Exact
       else
-        raise ArgumentError, 'Invalid segment'
+        raise ArgumentError, "Invalid segment: #{segment}"
       end
 
       klass.new(segment: segment, maximum: maximum, minimum: minimum)
